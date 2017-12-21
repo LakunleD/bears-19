ab7358f56f77bacbade53ac331903fe1291a2d75
Olakunle Dosunmu <olakunledosunmu@gmail.com>
2017-12-21 11:13:48 +0100

    changed express to hapi

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-let router = express.Router();
-
-/* GET home page. */
-router.get('/', function (req, res, next) {
-    res.send('Hello World!');
-});
-
-module.exports = router;
